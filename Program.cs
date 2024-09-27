0a48e6c43eb06b9baac37652094f6976e8f73de9
Usuario <Usuario@DESKTOP-9E3M4RJ>
2024-09-27 22:41:56 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace practice
+{
+    //esto es una clase, un conjunto de métodos
+    // static void es igual a una función
+    class Program
+     
+    {
+        //método principal del programa
+        static void Main(string[] args)
+        {
+            //Escribe en la consola
+            Console.WriteLine("Introduce dividendo"); 
+            //x es igual a lo que escribe el usuario en la siguiente linea (convertido de string a tipo float)
+            float x = float.Parse(Console.ReadLine());
+            Console.WriteLine("Introduce un divisor");
+            float y = float.Parse(Console.ReadLine());
+            // la llamada a la función (con los argumentos siendo las variables que se han definido)
+            Divide(x,y);
+               
+        }
+        //la función divide de x e y, y que salga en la consola
+        static void Divide(float x, float y)
+        {
+            Console.WriteLine(x / y);
+        }
+
+
+    }
+}
